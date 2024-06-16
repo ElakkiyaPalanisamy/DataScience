7fe80ccd15518566940aeb591d9f105bb88008cd
ElakkiyaPalanisamy <palanisamyelakkiya3@gmail.com>
2024-06-16 16:31:51 +0100

    Initialized the repo with cookiecutter data science structure

---
+++ DataScience/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Comparative analysis of breast cancer prediction:Deep Learning Vs Machine Learning algorithms with ensemble techniques',
+    author='Elakkiya Palanisamy',
+    license='MIT',
+)
